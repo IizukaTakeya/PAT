24342abf60c8720d84050ff4e47bca5d5d5ebcd1
IizukaTakeya <30492958+IizukaTakeya@users.noreply.github.com>
2020-07-07 11:46:18 +0800

    Add files via upload

---
+++ 1123.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+#include <string>
+#include <cstring>
+using namespace std;
+int main()
+{
+	string s;
+
+	return 0;
+}
